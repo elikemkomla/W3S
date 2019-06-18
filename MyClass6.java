61edc5cbdebbeabda4dab7c4ff662ae53182f2eb
elikemkomla <ebuame@gmail.com>
2019-06-19 00:59:01 +0100

    number 11

---
+++ MyClass6.java
@@ -0,0 +1,7 @@
+public class MyClass6 {
+  public static void main(String[] args) {
+    Person3 myObj = new Person3();
+    myObj.setName("John"); // Set the value of the name variable to "John"
+    System.out.println(myObj.getName());
+  }
+}
