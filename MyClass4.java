985b7773eb2026046b93e3695dee33b0eb12f374
elikemkomla <ebuame@gmail.com>
2019-06-18 19:06:35 +0100

    number 7

---
+++ MyClass4.java
@@ -0,0 +1,10 @@
+public class MyClass4 {
+  int x; 
+  public MyClass4() {
+    x = 5;
+    }
+  public static void main(String[] args) {
+    MyClass myObj = new MyClass();
+      System.out.println(myObj.x); 
+  }
+}
