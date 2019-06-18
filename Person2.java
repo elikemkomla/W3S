1f1f314f8cb59ccf3b9d2e7642c2abfa23a5b63a
elikemkomla <ebuame@gmail.com>
2019-06-18 19:32:22 +0100

    number 9

---
+++ Person2.java
@@ -0,0 +1,11 @@
+abstract class Person2 {
+  public String fname = "John";
+  public int age = 24;
+  public abstract void study();
+ }
+  class Student extends Person {
+  public int graduationYear = 2018;
+  public void study() {
+  System.out.println("Studying all day long");
+  }
+}
